787e4c0ca5612e01c60edb17b7f8c0130eeccc0c
mathter <mathter@mail.ru>
2016-12-20 12:52:50 +0700

    LexicalAnalizer add Streem interface

--- lexical/src/main/java/biz/ostw/bottex/lexical/Lexeme.java
+++ lexical/src/main/java/biz/ostw/bottex/lexical/Lexeme.java
@@ -1,9 +1,11 @@
 package biz.ostw.bottex.lexical;
 
+import java.io.Serializable;
+
 /**
  * @author mathter
  */
-public interface Lexeme extends CharSequence
+public interface Lexeme extends CharSequence, Serializable
 {
     public Type getType();
 
--- lexical/src/test/java/biz/ostw/bottex/lexical/simple/LexicalAnalizerTest.java
+++ lexical/src/test/java/biz/ostw/bottex/lexical/simple/LexicalAnalizerTest.java
@@ -2,6 +2,8 @@ package biz.ostw.bottex.lexical.simple;
 
 import java.io.InputStreamReader;
 import java.io.LineNumberReader;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -34,6 +36,27 @@ public class LexicalAnalizerTest
         {
             Lexeme lexeme = lexicalAnalizer.next();
             Assert.assertEquals( line, lexeme.toString() );
+        }
+    }
+
+    @Test
+    public void testStreemText() throws Exception
+    {
+        LexicalAnalizerProvider lexicalAnalizerProvider = new CommonLexicalProviderFactory().newInstance( "SimpleAnalizerProvider" );
+        LexicalAnalizerBuilder lexicalAnalizerBuilder = lexicalAnalizerProvider.getBuilder();
+        LexicalAnalizer lexicalAnalizer = lexicalAnalizerBuilder
+            .build( new InputStreamReader( LexicalAnalizerTest.class.getResourceAsStream( "test.txt" ) ), null );
+
+        LineNumberReader reader = new LineNumberReader(
+            new InputStreamReader( LexicalAnalizerTest.class.getResourceAsStream( "etalon.txt" ) ) );
+
+        String line;
+        List< String > lexemes = new ArrayList<>();
+        lexicalAnalizer.streem().forEach( l -> lexemes.add( l.toString() ) );
+
+        for ( int i = 0; ( line = reader.readLine() ) != null; i ++ )
+        {
+            Assert.assertEquals( line, lexemes.get( i ) );
         }
     }
 }
